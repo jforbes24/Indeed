89c18b3eca9f518efbe655d46ea997d786025b24
James Forbes <64039648+jforbes24@users.noreply.github.com>
2021-08-03 23:36:10 +0100

    Update jobSearch.py

--- jobSearch.py
+++ jobSearch.py
@@ -3,48 +3,102 @@ import csv
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+import time
+import pandas as pd
 
 # format url
+
+baseUrl = 'https://uk.indeed.com'
+
 def get_url(position, location):
     # generate url from position and location
-    template = 'https://uk.indeed.com/jobs?q={}&l={}'
+    template = baseUrl + '/jobs?q={}&l={}'
     url = template.format(position, location)
     return url
 
-url = get_url('senior analyst', 'london')
+# Extract job data from a single record
+def get_record(card):
+    # get position
+    try:
+        job_title = card.h2.text.strip().lstrip('new')
+    except AttributeError:
+        job_title = ''
+    # get company
+    try:
+        company = card.find('span', class_='companyName').text.strip('\n')
+    except AttributeError:
+        company = ''
+    # get frequency
+    try:
+        new = card.find('span', class_='label').text.strip()
+    except AttributeError:
+        new = ''
+    # get url
+    try:
+        atag = card.find('a')
+        job_url = baseUrl + atag.get('href')
+    except AttributeError:
+        job_url = ''
+    # get job location
+    try:
+        job_loc = card.find('div', class_='companyLocation').text.strip()
+    except AttributeError:
+        job_loc = ''
+    # get summary
+    try:
+        jd = card.find('div', class_='job-snippet').text.strip('\n')
+    except AttributeError:
+        jd = ''
+    # get salary
+    try:
+        salary = card.find('span', class_='salary-snippet').text
+    except AttributeError:
+        salary = ''
+    # get rating
+    try:
+        rating = card.find('span', class_='ratingNumber').text.strip()
+    except AttributeError:
+        rating = ''
+    # get post date
+    try:
+        post_date = card.find('span', class_='date').text.strip()
+    except AttributeError:
+        post_date = ''
+    today = datetime.today().strftime('%Y-%m-%d')
 
-# create session
-s = requests.Session()
-response = s.get(url)
-print(response)
+    record = (job_title, company, new, job_url, job_loc, jd, salary, rating, post_date, today)
 
-soup = BeautifulSoup(response.content, 'html.parser')
-cards = soup.find_all('div', class_='job_seen_beacon')
-card = cards[0]
+    return record
 
+def main(position, location):
+    # run main program
+    records = []
+    url = get_url(position, location)
 
+    # extract the job data
+    while True:
+        # create session
+        s = requests.Session()
+        response = s.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        cards = soup.find_all('div', class_='job_seen_beacon')
 
-# get position
-job_title = card.h2.text
-print(job_title)
+        for card in cards:
+            record = get_record(card)
+            records.append(record)
+            time.sleep(1.5)
 
-# get company
-company = card.find('span', class_='companyName').text.lstrip('New')
-print(company)
+        # pagination
+        try:
+            url = baseUrl + soup.find('a', attrs={'aria-label': "Next"}).get('href')
+        except AttributeError:
+            break
 
-# get frequency
-new = card.find('span', class_='label').text.strip()
-print(new)
+    # save the job data
+    with open('results.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['job_title', 'company', 'new', 'job_url', 'job_loc', 'jd', 'salary','rating', 'post_date', 'today'])
+        writer.writerows(records)
 
-# get url
-atag = card.find('a')
-job_url = 'https://uk.indeed.com' + atag.get('href')
-print(job_url)
-
-# get job location
-job_loc = card.find('div', class_='companyLocation').text.strip()
-print(job_loc)
-
-# get summary
-jd = card.find('div', class_='job-snippet').text.strip('\n')
-print(jd)
+# run the main program
+main('senior analyst', 'london')
